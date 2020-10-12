35fd998e71c65388d002429bbd3718d21156ac07
Sebastian Bolanos <sebasheston@protonmail.com>
2020-10-12 12:12:20 +0200

    Remove extra variable

--- ops/deploy.sh
+++ ops/deploy.sh
@@ -6,7 +6,7 @@ export BRIDGE_URL=$(grep BRIDGE_URL config | cut -f2 -d=)
 export CERTBOT_EMAIL=$(grep CERTBOT_EMAIL config | cut -f2 -d=)
 export CLOUDFLARE=$(grep CLOUDFLARE config | cut -f2 -d=)
 
-run="docker stack deploy $run $PROJECT -c ops/docker-compose.yml -c ops/docker-compose.prod.yml "
+run="docker stack deploy $PROJECT -c ops/docker-compose.yml -c ops/docker-compose.prod.yml "
 if [[ $CLOUDFLARE != false ]]; then
   run="${run} -c /tmp/${PROJECT}.secrets.yml"
 fi
